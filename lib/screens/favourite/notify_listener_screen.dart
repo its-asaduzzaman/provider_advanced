90e74c3779ad73d6111d5f117c1d6b98e748df58
Asaduzzaman_mac <samrat.asad@gmail.com>
2022-12-12 22:19:56 +0600

    make stateless to statefull exmp 02

--- lib/screens/favourite/notify_listener_screen.dart
+++ lib/screens/favourite/notify_listener_screen.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NotifyListenerScreen extends StatelessWidget {
   NotifyListenerScreen({Key? key}) : super(key: key);
 
   final ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  final ValueNotifier<bool> _toggle = ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
     print("build");
     return Scaffold(
-      body: Center(
-          child: ValueListenableBuilder(
-              valueListenable: _counter,
-              builder: (context, value, child) {
-                return Text(
-                  _counter.value.toString(),
-                  style: const TextStyle(
-                    fontSize: 50,
-                  ),
-                );
-              })),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ValueListenableBuilder(
+                valueListenable: _toggle,
+                builder: (context, value, child) {
+                  return TextField(
+                    obscureText: _toggle.value,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          _toggle.value = !_toggle.value;
+                        },
+                        child: Icon(
+                          _toggle.value
+                              ? Icons.visibility_off
+                              : Icons.remove_red_eye,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+            Center(
+                child: ValueListenableBuilder(
+                    valueListenable: _counter,
+                    builder: (context, value, child) {
+                      return Text(
+                        _counter.value.toString(),
+                        style: const TextStyle(
+                          fontSize: 50,
+                        ),
+                      );
+                    })),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _counter.value++;
